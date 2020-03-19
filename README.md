8915a19c1cd56a18f9ab25b5d128aface6e70cdf
findnr <504875043@qq.com>
2020-03-19 08:00:52 +0800

    20200319更新CreateNodeTree()

--- README.md
+++ README.md
@@ -22,6 +22,7 @@ let data =[
 第二个参数是 id 的名称:test_id
 第三个参数是从第几级开始生成树，这里是 0
 第四个参数产生子节点时生成的名称，这里是 children
-CreateNodeTree(data=[], idName = "id",topId=0,nodesName="children");
+第四个参数产生子节点时生成的名称，这里是 children
+CreateNodeTree(data=[], idName = "test_id",topId=0,nodesName="children",parentName="parent_id");
 返回的结果是：
 newData=[{test_id:'1',name:"书",parent_id:0,children:[{test_id:'2',name:"书",parent_id:1,children:[{test_id:'3',name:"书",parent_id:2}]}]},{test_id:'4',name:"书",parent_id:0}]
--- src/lib/CreateNodeTree.js
+++ src/lib/CreateNodeTree.js
@@ -1,29 +1,35 @@
-module.exports=function CreateNodeTree(data=[], idName = "id",topId=0,nodesName="children"){
-    if (!data.length) {
-        return data;
+module.exports = function CreateNodeTree(
+  data = [],
+  idName = "id",
+  topId = 0,
+  nodesName = "children",
+  parentName = "parentId"
+) {
+  if (!data.length) {
+    return data;
+  }
+  function findChild(parentId) {
+    let obj = [];
+    data.forEach(v => {
+      if (v[parentName] * 1 == parentId * 1) {
+        obj.push(v);
       }
-      function findChild(parentId) {
-        let obj = [];
-        data.forEach(v => {
-          if (v.parentId * 1 == parentId * 1) {
-            obj.push(v);
-          }
-        });
-        return obj;
+    });
+    return obj;
+  }
+  function tree(pid) {
+    let obj = findChild(pid);
+    if (obj.length == 0) {
+      return null;
+    }
+    obj.forEach(v => {
+      let buildTree = tree(v[idName]);
+      if (null != buildTree) {
+        v[nodesName] = buildTree;
       }
-      function tree(pid) {
-        let obj = findChild(pid);
-        if (obj.length == 0) {
-          return null;
-        }
-        obj.forEach(v => {
-          let buildTree = tree(v[idName]);
-          if (null != buildTree) {
-            v[nodesName] = buildTree;
-          }
-        });
-        return obj;
-      }
-      let arr = tree(topId);
-      return arr;
-}
+    });
+    return obj;
+  }
+  let arr = tree(topId);
+  return arr;
+};
