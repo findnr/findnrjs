416324016d353d768de044f68b00f4730558bc4f
504875043@qq.com <504875043@qq.com>
2020-05-20 22:28:25 +0800

    time20200520

--- src/lib/Time.js
+++ src/lib/Time.js
@@ -1,15 +1,40 @@
 module.exports = {
-  data: {},
-  getFormat() {
+  data: {
+    now: new Date(),
+  },
+  getFormat(obj) {
+    let o = {
+      time: 0,
+      action: 1,
+      type: "s",
+    };
+    if (obj.time) o.time = obj.time;
+    if (obj.action) o.action = obj.action;
+    if (obj.type) o.type = obj.time;
     return this;
   },
-  getYear() {
-    return new Date().getFullYear();
+  _Y() {
+    return this.data.getFullYear();
   },
-  getMonth: () => {
-    return new Date().getMonth() + 1;
+  _M: () => {
+    return this.data.getMonth() + 1;
   },
-  getDate: () => {
-    return new Date().getDate();
-  }
+  _D: () => {
+    return this.data.getDate();
+  },
+  _W: () => {
+    return this.data.getDay();
+  },
+  _H: () => {
+    return this.data.getHours();
+  },
+  _I: () => {
+    return this.data.getMinutes();
+  },
+  _S: () => {
+    return this.data.getSeconds();
+  },
+  _MS: () => {
+    return this.data.getMilliseconds();
+  },
 };
