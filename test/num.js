7d75eb6892062a116befb12eb0c914d602dbdb08
findnr <cym504875043@gmial.com>
2024-10-12 15:07:33 +0800

    202410121507

--- test/num.js
+++ test/num.js
@@ -1,10 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2021-08-04 21:56:41
- * @LastEditTime: 2021-08-10 16:32:06
- * @LastEditors: 程英明
+ * @LastEditTime: 2024-10-12 11:41:37
+ * @LastEditors: findnr
  * @Description: In User Settings Edit
  * @FilePath: \findnrjs\test\num.js
  */
 const { Num } = require("../src/index");
-console.log(Num.encode.MoneyZh(123));
+// console.log(Num.encode.MoneyZh(123));
+// console.log(Num.check.IdCard(''));
+// console.log(Num.check.UnitCard('51520000736604536T'));
